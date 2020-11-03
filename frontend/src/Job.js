a3de866ba2b56999155c4eab9eeeabd843e0bc3a
IvanTurbin <ivan.turbin@gmail.com>
2020-11-03 02:40:09 +0200

    Add logic for max amount of boxes
    1.5 hours , debuging, reading docs and css

--- frontend/src/Job.js
+++ frontend/src/Job.js
@@ -3,11 +3,10 @@ import "./css/radiator-cell.css"
 
 const Job = (props) => {
   const refCellWidth = useRef(null);
-  const [amountOfSquares, setAmountOfSquares] = useState([])
+  const [amountOfSquares, setAmountOfSquares] = useState(0)
 
   useEffect ( () => {
-    console.log(refCellWidth.current.offsetWidth)
-    setAmountOfSquares(refCellWidth.current.offsetWidth / 19);
+    setAmountOfSquares(refCellWidth.current.offsetWidth / 34);
   }, [refCellWidth]);
 
   /*
@@ -19,13 +18,19 @@ const Job = (props) => {
           )}
    */
 
+  const StatusBoxes = () =>{
+    let boxes = []
+    for (let i = 0; i < amountOfSquares; i++) {
+      boxes = boxes.concat(<div className={"status-box"} style={{background: props.status || "gray"}}/>)
+    }
+    return boxes
+  }
+
   return(
     <div className={"cell"} style={{background: props.color || "blue", margin: 5, flexGrow: props.grow || 1, order: props.order}}>
-      <p>{props.text}</p>
+      <h2 className={"title"}>{props.text}</h2>
       <div className={"status-box-container"} ref={refCellWidth}>
-        <div className={"status-box"} style={{background: props.status || "gray"}}/>
-        <div className={"status-box"} style={{background: props.status || "gray"}}/>
-        <div className={"status-box"} style={{background: props.status || "gray"}}/>
+        <StatusBoxes/>
       </div>
     </div>
   )
