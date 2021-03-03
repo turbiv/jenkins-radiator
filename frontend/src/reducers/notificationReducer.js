42d82b369693af1729088e41276c47dfe4cf026f
IvanTurbin <ivan.turbin@gmail.com>
2021-03-03 23:50:59 +0200

    The actual notification changes...

--- frontend/src/reducers/notificationReducer.js
+++ frontend/src/reducers/notificationReducer.js
@@ -1,12 +1,14 @@
 import React from "react"
+import "../css/notification.css"
 const initalState = [];
 
-export const createNotification = (notification, timeout = 7) => {
+export const createNotification = (notification, status = "info", timeout = 7) => {
   return async dispatch => {
     dispatch({
       type: "SET_NOTIFICATION",
       data: {
-        notification
+        notification,
+        status
       }
     });
     setTimeout(()=>{
@@ -25,12 +27,20 @@ const reducer = (state = initalState, action) =>{
 
   switch (action.type) {
     case "SET_NOTIFICATION":
-      return [...state, <div>{action.data.notification}</div>];
+      switch (action.data.status) {
+        case "fail":
+          return [...state, <div className={"notification-content"} style={{backgroundColor: "crimson"}}>{action.data.notification}</div>];
+        case "success":
+          return [...state, <div className={"notification-content"} style={{backgroundColor: "lightgreen"}}>{action.data.notification}</div>];
+        case "info":
+          return [...state, <div className={"notification-content"} style={{backgroundColor: "lightblue"}}>{action.data.notification}</div>];
+        default:
+      }
     case "REMOVE_NOTIFICATION":
       return state.slice(1);
     default:
-
   }
+
   return state
 };
 
