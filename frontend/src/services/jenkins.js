a5baea1e49f28d22b259aa1ed51f35194aa4672f
IvanTurbin <ivan.turbin@gmail.com>
2021-03-05 02:32:16 +0200

    Change baseurl for production

--- frontend/src/services/jenkins.js
+++ frontend/src/services/jenkins.js
@@ -1,19 +1,21 @@
 import axios from "axios"
 
-export const getAllJenkins = (jenkinsUrl, buildAmount) => {
-  const request = axios.get( `http://localhost:3003/api/jenkins/`)
+const baseurl = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? "http://localhost:3003/api" : "https://radiator-backend.herokuapp.com/api"
+
+export const getAllJenkins = () => {
+  const request = axios.get( `${baseurl}/jenkins/`)
   return request.then(response => response.data)
     .catch(error => Promise.reject(error.response.status))
 };
 
 export const getJenkinsById = async (id) => {
-  const request = axios.get(`http://localhost:3003/api/jenkins/${id}`)
+  const request = axios.get(`${baseurl}/jenkins/${id}`)
   return request.then(response => response.data)
     .catch(error => Promise.reject(error.response.status))
 }
 
 export const postNewJenkins = async (payload) => {
-  const request = axios.post(`http://localhost:3003/api/jenkins/`, payload)
+  const request = axios.post(`${baseurl}/jenkins/`, payload)
   return request.then(() => Promise.resolve())
     .catch(error => Promise.reject(error.response.status))
 }
